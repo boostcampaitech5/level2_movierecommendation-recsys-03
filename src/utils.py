82a05221bddbea396e66f26f497f3da8197fd73b
2jun0 <2jun0@gmail.com>
2023-06-06 17:16:07 +0000

    feat: add utils

---
+++ src/utils.py
@@ -0,0 +1,289 @@
+import json
+import math
+import os
+import random
+
+import numpy as np
+import pandas as pd
+import torch
+from scipy.sparse import csr_matrix
+
+
+def set_seed(seed):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    # some cudnn methods can be random even after fixing the seed
+    # unless you tell it to be deterministic
+    torch.backends.cudnn.deterministic = True
+
+
+def check_path(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(f"{path} created")
+
+
+def neg_sample(item_set: set, item_size):
+    neg_items = set(x for x in range(1, item_size)) - item_set
+    return random.choice(neg_items)
+
+    # item = random.randint(1, item_size - 1)
+    # while item in item_set:
+    #     item = random.randint(1, item_size - 1)
+    # return item
+
+
+def kmax_pooling(x, dim, k):
+    index = x.topk(k, dim=dim)[1].sort(dim=dim)[0]
+    return x.gather(dim, index).squeeze(dim)
+
+
+def avg_pooling(x, dim):
+    return x.sum(dim=dim) / x.size(dim)
+
+
+def generate_rating_matrix_valid(user_seq, num_users, num_items):
+    # three lists are used to construct sparse matrix
+    row = []
+    col = []
+    data = []
+    for user_id, item_list in enumerate(user_seq):
+        for item in item_list[:-2]:  #
+            row.append(user_id)
+            col.append(item)
+            data.append(1)
+
+    row = np.array(row)
+    col = np.array(col)
+    data = np.array(data)
+    rating_matrix = csr_matrix((data, (row, col)), shape=(num_users, num_items))
+
+    return rating_matrix
+
+
+def generate_rating_matrix_test(user_seq, num_users, num_items):
+    # three lists are used to construct sparse matrix
+    row = []
+    col = []
+    data = []
+    for user_id, item_list in enumerate(user_seq):
+        for item in item_list[:-1]:  #
+            row.append(user_id)
+            col.append(item)
+            data.append(1)
+
+    row = np.array(row)
+    col = np.array(col)
+    data = np.array(data)
+    rating_matrix = csr_matrix((data, (row, col)), shape=(num_users, num_items))
+
+    return rating_matrix
+
+
+def generate_rating_matrix_submission(user_seq, num_users, num_items):
+    # three lists are used to construct sparse matrix
+    row = []
+    col = []
+    data = []
+    for user_id, item_list in enumerate(user_seq):
+        for item in item_list[:]:  #
+            row.append(user_id)
+            col.append(item)
+            data.append(1)
+
+    row = np.array(row)
+    col = np.array(col)
+    data = np.array(data)
+    rating_matrix = csr_matrix((data, (row, col)), shape=(num_users, num_items))
+
+    return rating_matrix
+
+
+def generate_submission_file(data_file, preds):
+    rating_df = pd.read_csv(data_file)
+    users = rating_df["user"].unique()
+
+    result = []
+
+    for index, items in enumerate(preds):
+        for item in items:
+            result.append((users[index], item))
+
+    pd.DataFrame(result, columns=["user", "item"]).to_csv("output/submission.csv", index=False)
+
+
+def get_user_seqs(data_file):
+    rating_df = pd.read_csv(data_file)
+    lines = rating_df.groupby("user")["item"].apply(list)
+    user_seq = []
+    item_set = set()
+    for line in lines:
+        items = line
+        user_seq.append(items)
+        item_set = item_set | set(items)
+    max_item = max(item_set)
+
+    num_users = len(lines)
+    num_items = max_item + 2
+
+    valid_rating_matrix = generate_rating_matrix_valid(user_seq, num_users, num_items)
+    test_rating_matrix = generate_rating_matrix_test(user_seq, num_users, num_items)
+    submission_rating_matrix = generate_rating_matrix_submission(user_seq, num_users, num_items)
+    return (
+        user_seq,
+        max_item,
+        valid_rating_matrix,
+        test_rating_matrix,
+        submission_rating_matrix,
+    )
+
+
+def get_user_seqs_long(data_file):
+    rating_df = pd.read_csv(data_file)
+    lines = rating_df.groupby("user")["item"].apply(list)
+    user_seq = []
+    long_seq = []
+    item_set = set()
+    for line in lines:
+        items = line
+        long_seq.extend(items)
+        user_seq.append(items)
+        item_set = item_set | set(items)
+    max_item = max(item_set)
+
+    return user_seq, max_item, long_seq
+
+
+def get_item2attr_json(data_file):
+    item2attr = json.loads(open(data_file).readline())
+    attr_set = set()
+    for item, attrs in item2attr.items():
+        attr_set = attr_set | set(attrs)
+    attr_size = max(attr_set)
+    return item2attr, attr_size
+
+
+def get_metric(pred_list, topk=10):
+    NDCG = 0.0
+    HIT = 0.0
+    MRR = 0.0
+    # [batch] the answer's rank
+    for rank in pred_list:
+        MRR += 1.0 / (rank + 1.0)
+        if rank < topk:
+            NDCG += 1.0 / np.log2(rank + 2.0)
+            HIT += 1.0
+    return HIT / len(pred_list), NDCG / len(pred_list), MRR / len(pred_list)
+
+
+def precision_at_k_per_sample(actual, predicted, topk):
+    num_hits = 0
+    for place in predicted:
+        if place in actual:
+            num_hits += 1
+    return num_hits / (topk + 0.0)
+
+
+def precision_at_k(actual, predicted, topk):
+    sum_precision = 0.0
+    num_users = len(predicted)
+    for i in range(num_users):
+        act_set = set(actual[i])
+        pred_set = set(predicted[i][:topk])
+        sum_precision += len(act_set & pred_set) / float(topk)
+
+    return sum_precision / num_users
+
+
+def recall_at_k(actual, predicted, topk):
+    sum_recall = 0.0
+    num_users = len(predicted)
+    true_users = 0
+    for i in range(num_users):
+        act_set = set(actual[i])
+        pred_set = set(predicted[i][:topk])
+        if len(act_set) != 0:
+            sum_recall += len(act_set & pred_set) / float(len(act_set))
+            true_users += 1
+    return sum_recall / true_users
+
+
+def apk(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+    This function computes the average precision at k between two lists of
+    items.
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+    """
+    if len(predicted) > k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i, p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i + 1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
+
+
+def mapk(actual, predicted, k=10):
+    """
+    Computes the mean average precision at k.
+    This function computes the mean average prescision at k between two lists
+    of lists of items.
+    Parameters
+    ----------
+    actual : list
+             A list of lists of elements that are to be predicted
+             (order doesn't matter in the lists)
+    predicted : list
+                A list of lists of predicted elements
+                (order matters in the lists)
+    k : int, optional
+        The maximum number of predicted elements
+    Returns
+    -------
+    score : double
+            The mean average precision at k over the input lists
+    """
+    return np.mean([apk(a, p, k) for a, p in zip(actual, predicted)])
+
+
+def ndcg_k(actual, predicted, topk):
+    res = 0
+    for user_id in range(len(actual)):
+        k = min(topk, len(actual[user_id]))
+        idcg = idcg_k(k)
+        dcg_k = sum([int(predicted[user_id][j] in set(actual[user_id])) / math.log(j + 2, 2) for j in range(topk)])
+        res += dcg_k / idcg
+    return res / float(len(actual))
+
+
+# Calculates the ideal discounted cumulative gain at k
+def idcg_k(k):
+    res = sum([1.0 / math.log(i + 2, 2) for i in range(k)])
+    if not res:
+        return 1.0
+    else:
+        return res
