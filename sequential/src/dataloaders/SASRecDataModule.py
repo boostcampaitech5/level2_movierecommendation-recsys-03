f7a9b0f100ce952fccd2acc0c360fbbaaab455de
2jun0 <2jun0@gmail.com>
2023-06-11 13:41:24 +0000

    feat: impliment SASRecKFoldDataModule

--- sequential/src/dataloaders/SASRecDataModule.py
+++ sequential/src/dataloaders/SASRecDataModule.py
@@ -3,6 +3,7 @@ from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 from src.config import Config
 from src.utils import get_user_seqs, get_user_seqs_long, get_item2attr_json
 from src.datasets import SASRecDataset
+from src.dataloaders import KFoldDataModule, KFoldDataModuleContainer
 
 
 class SASRecDataModule(L.LightningDataModule):
@@ -87,3 +88,62 @@ class SASRecDataModule(L.LightningDataModule):
         submission_dataset = SASRecDataset(config=self.config, data=self.submission_data, user_seq=self.user_seq)
         submission_sampler = SequentialSampler(submission_dataset)
         return DataLoader(submission_dataset, sampler=submission_sampler, batch_size=self.batch_size, num_workers=0)
+
+
+class SASRecKFoldDataModule(SASRecDataModule, KFoldDataModule):
+    def __init__(self, config: Config, fold: int):
+        SASRecDataModule.__init__(config)
+        self.fold = fold
+
+    def __split_data(self, data: str, idx: int) -> dict:
+        if idx == 0:
+            new_data = {
+                "input_ids": [user[:idx] for user in data["input_ids"]],
+                "target_pos": [user[:idx] for user in data["target_pos"]],
+                "answers": data["answers"],
+            }
+        else:
+            new_data = {
+                "input_ids": [user[:idx] for user in data["input_ids"]],
+                "target_pos": [user[:idx] for user in data["target_pos"]],
+                "answers": [[user[idx]] for user in data["input_ids"]],
+            }
+
+        return new_data
+
+    def setup(self, stage=None):
+        self.n_user = len(self.user_seq)
+
+        if stage == "fit" or stage is None:
+            idx = self.fold + 1
+            train_idx = -1 * idx
+            valid_idx = idx - 1
+
+            # train
+            self.train_data = {
+                "input_ids": [seq[:-2] for seq in self.user_seq],
+                "target_pos": [seq[1:-1] for seq in self.user_seq],
+                "answers": [[seq[-2]] for seq in self.user_seq],
+            }
+
+            self.valid_data = self.__split_data(self.train_data, valid_idx)
+            self.train_data = self.__split_data(self.train_data, train_idx)
+
+        elif stage == "test" or stage is None:
+            # test
+            self.test_data = {
+                "input_ids": [seq[:-1] for seq in self.user_seq],
+                "target_pos": [seq[1:] for seq in self.user_seq],
+                "answers": [[seq[-1]] for seq in self.user_seq],
+            }
+
+        elif stage == "predict" or stage is None:
+            self.submission_data = {"input_ids": self.user_seq, "target_pos": self.user_seq, "answers": [[0] for _ in range(self.n_user)]}
+
+
+class SASRecKFoldDataModuleContainer(KFoldDataModuleContainer):
+    def __init__(self, config: Config):
+        self.config = config
+
+    def kfold_data_module(self, fold: int) -> KFoldDataModule:
+        return SASRecKFoldDataModule(self.config, fold)
