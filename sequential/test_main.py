e4875e56999ac5420020c06f4908bd0d05bc2b3b
2jun0 <2jun0@gmail.com>
2023-06-12 02:36:10 +0000

    test: impliment test code for sequential

---
+++ sequential/test_main.py
@@ -0,0 +1,25 @@
+import hydra
+import os
+import omegaconf
+from src.config import Config
+from main import main
+
+
+def test_main(*args, **kwargs):
+    @hydra.main(version_base="1.2", config_path="configs", config_name="test.yaml")
+    def inner_main(config: Config) -> None:
+        for model_path in os.listdir("configs/model.test"):
+            model_path = os.path.join("configs/model.test", model_path)
+
+            model_config = omegaconf.OmegaConf.load(model_path)
+            config.model = model_config
+
+            for trainer_path in os.listdir("configs/trainer.test"):
+                trainer_path = os.path.join("configs/trainer.test", trainer_path)
+
+                trainer_config = omegaconf.OmegaConf.load(trainer_path)
+                config.trainer = trainer_config
+
+                main(config)
+
+    inner_main()
