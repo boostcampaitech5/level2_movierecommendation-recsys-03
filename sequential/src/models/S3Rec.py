22c291f9b15c3914bf8e599b3aaa30b19fcb08ce
2jun0 <2jun0@gmail.com>
2023-06-13 13:30:14 +0000

    refactor: separate s3rec module from s3rec model

--- sequential/src/models/S3Rec.py
+++ sequential/src/models/S3Rec.py
@@ -12,136 +12,24 @@ class S3Rec(L.LightningModule):
     def __init__(self, config: Config):
         super().__init__()
         self.config = config
-        self.sasrec = modules.SASRec(config)
         self.mask_id = config.data.mask_id
-        self.attr_size = config.data.attr_size
         self.aap_weight = config.trainer.aap_weight
         self.mip_weight = config.trainer.mip_weight
         self.map_weight = config.trainer.map_weight
         self.sp_weight = config.trainer.sp_weight
-        self.hidden_size = self.sasrec.hidden_size
+
+        self.base_module = modules.SASRec(config)
+        self.s3rec = modules.S3Rec(config, self.base_module)
 
         self.training_step_outputs = []
 
-        self.attr_embeddings = nn.Embedding(self.attr_size, self.hidden_size, padding_idx=0)
-
         # add unique dense layer for 4 losses respectively
-        self.aap_norm = nn.Linear(self.hidden_size, self.hidden_size)
-        self.mip_norm = nn.Linear(self.hidden_size, self.hidden_size)
-        self.map_norm = nn.Linear(self.hidden_size, self.hidden_size)
-        self.sp_norm = nn.Linear(self.hidden_size, self.hidden_size)
         self.criterion = nn.BCELoss(reduction="none")
-        self.apply(self.sasrec.init_weights)
-
-    # AAP
-    def associated_attr_prediction(self, seq_output, attr_embedding) -> torch.Tensor:
-        """
-        :param seq_output: [B L H]
-        :param attr_embedding: [arribute_num H]
-        :return: scores [B*L tag_num]
-        """
-        seq_output = self.aap_norm(seq_output)  # [B L H]
-        seq_output = seq_output.view([-1, self.sasrec.hidden_size, 1])  # [B*L H 1]
-        # [tag_num H] [B*L H 1] -> [B*L tag_num 1]
-        score = torch.matmul(attr_embedding, seq_output)
-        return torch.sigmoid(score.squeeze(-1))  # [B*L tag_num]
-
-    # MIP sample neg items
-    def masked_item_prediction(self, seq_output, target_item) -> torch.Tensor:
-        """
-        :param seq_output: [B L H]
-        :param target_item: [B L H]
-        :return: scores [B*L]
-        """
-        seq_output = self.mip_norm(seq_output.view([-1, self.sasrec.hidden_size]))  # [B*L H]
-        target_item = target_item.view([-1, self.sasrec.hidden_size])  # [B*L H]
-        score = torch.mul(seq_output, target_item)  # [B*L H]
-        return torch.sigmoid(torch.sum(score, -1))  # [B*L]
-
-    # MAP
-    def masked_attr_prediction(self, seq_output, attr_embedding) -> torch.Tensor:
-        seq_output = self.map_norm(seq_output)  # [B L H]
-        seq_output = seq_output.view([-1, self.sasrec.hidden_size, 1])  # [B*L H 1]
-        # [tag_num H] [B*L H 1] -> [B*L tag_num 1]
-        score = torch.matmul(attr_embedding, seq_output)
-        return torch.sigmoid(score.squeeze(-1))  # [B*L tag_num]
-
-    # SP sample neg segment
-    def segment_prediction(self, context, segment) -> torch.Tensor:
-        """
-        :param context: [B H]
-        :param segment: [B H]
-        :return:
-        """
-        context = self.sp_norm(context)
-        score = torch.mul(context, segment)  # [B H]
-        return torch.sigmoid(torch.sum(score, dim=-1))  # [B]
-
-    def forward(
-        self,
-        masked_item_seq,
-        pos_items,
-        neg_items,
-        masked_segment_seq,
-        pos_segment,
-        neg_segment,
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        # Encode masked seq
-        seq_emb = self.sasrec.make_seq_embedding(masked_item_seq)
-        seq_mask = (masked_item_seq == 0).float() * -1e8
-        seq_mask = torch.unsqueeze(torch.unsqueeze(seq_mask, 1), 1)
-
-        encoded_layers = self.sasrec.item_encoder(seq_emb, seq_mask, output_all_encoded_layers=True)
-        # [B L H]
-        seq_output = encoded_layers[-1]
-
-        attr_embeddings = self.attr_embeddings.weight
-        # AAP
-        aap_score = self.associated_attr_prediction(seq_output, attr_embeddings)
-
-        # MIP
-        pos_item_embs = self.sasrec.item_embeddings(pos_items)
-        neg_item_embs = self.sasrec.item_embeddings(neg_items)
-        pos_score = self.masked_item_prediction(seq_output, pos_item_embs)
-        neg_score = self.masked_item_prediction(seq_output, neg_item_embs)
-        mip_distance = torch.sigmoid(pos_score - neg_score)
-
-        # MAP
-        map_score = self.masked_attr_prediction(seq_output, attr_embeddings)
-
-        # SP
-        # segment context
-        segment_context = self.sasrec.make_seq_embedding(masked_segment_seq)
-        segment_mask = (masked_segment_seq == 0).float() * -1e8
-        segment_mask = torch.unsqueeze(torch.unsqueeze(segment_mask, 1), 1)
-        segment_encoded_layers = self.sasrec.item_encoder(segment_context, segment_mask, output_all_encoded_layers=True)
-
-        # take the last position hidden as the context
-        segment_context = segment_encoded_layers[-1][:, -1, :]  # [B H]
-        # pos_segment
-        pos_segment_emb = self.sasrec.make_seq_embedding(pos_segment)
-        pos_segment_mask = (pos_segment == 0).float() * -1e8
-        pos_segment_mask = torch.unsqueeze(torch.unsqueeze(pos_segment_mask, 1), 1)
-        pos_segment_encoded_layers = self.sasrec.item_encoder(pos_segment_emb, pos_segment_mask, output_all_encoded_layers=True)
-        pos_segment_emb = pos_segment_encoded_layers[-1][:, -1, :]
-
-        # neg_segment
-        neg_segment_emb = self.sasrec.make_seq_embedding(neg_segment)
-        neg_segment_mask = (neg_segment == 0).float() * -1e8
-        neg_segment_mask = torch.unsqueeze(torch.unsqueeze(neg_segment_mask, 1), 1)
-        neg_segment_encoded_layers = self.sasrec.item_encoder(neg_segment_emb, neg_segment_mask, output_all_encoded_layers=True)
-        neg_segment_emb = neg_segment_encoded_layers[-1][:, -1, :]  # [B H]
-
-        pos_segment_score = self.segment_prediction(segment_context, pos_segment_emb)
-        neg_segment_score = self.segment_prediction(segment_context, neg_segment_emb)
-
-        sp_distance = torch.sigmoid(pos_segment_score - neg_segment_score)
-
-        return aap_score, mip_distance, map_score, sp_distance
+        self.apply(self.base_module.init_weights)
 
     def compute_loss(self, aap_score, mip_distance, map_score, sp_distance, attrs, masked_item_seq):
         ## AAP
-        aap_loss = self.criterion(aap_score, attrs.view(-1, self.attr_size).float())
+        aap_loss = self.criterion(aap_score, attrs.view(-1, self.s3rec.attr_size).float())
         # only compute loss at non-masked position
         aap_mask = (masked_item_seq != self.mask_id).float() * (masked_item_seq != 0).float()
         aap_loss = torch.sum(aap_loss * aap_mask.flatten().unsqueeze(-1))
@@ -152,7 +40,7 @@ class S3Rec(L.LightningModule):
         mip_loss = torch.sum(mip_loss * mip_mask.flatten())
 
         ## MAP
-        map_loss = self.criterion(map_score, attrs.view(-1, self.attr_size).float())
+        map_loss = self.criterion(map_score, attrs.view(-1, self.s3rec.attr_size).float())
         map_mask = (masked_item_seq == self.mask_id).float()
         map_loss = torch.sum(map_loss * map_mask.flatten().unsqueeze(-1))
 
@@ -218,4 +106,15 @@ class S3Rec(L.LightningModule):
         self.training_step_outputs.clear()
 
     def save_pretrained_module(self, path) -> None:
-        self.sasrec.save(path)
+        self.base_module.save(path)
+
+    def forward(
+        self,
+        masked_item_seq,
+        pos_items,
+        neg_items,
+        masked_segment_seq,
+        pos_segment,
+        neg_segment,
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        return self.s3rec.forward(masked_item_seq, pos_items, neg_items, masked_segment_seq, pos_segment, neg_segment)
---
+++ sequential/src/modules/S3Rec.py
@@ -0,0 +1,126 @@
+import torch
+import torch.nn as nn
+from src.config import Config
+
+
+class S3Rec(nn.Module):
+    def __init__(self, config: Config, base_module: nn.Module):
+        super().__init__()
+        self.attr_size = config.data.attr_size
+        self.hidden_size = config.model.hidden_size
+
+        self.base_module = base_module
+        self.attr_embeddings = nn.Embedding(self.attr_size, self.hidden_size, padding_idx=0)
+
+        self.aap_norm = nn.Linear(self.hidden_size, self.hidden_size)
+        self.mip_norm = nn.Linear(self.hidden_size, self.hidden_size)
+        self.map_norm = nn.Linear(self.hidden_size, self.hidden_size)
+        self.sp_norm = nn.Linear(self.hidden_size, self.hidden_size)
+
+        self.base_module = base_module
+
+    # AAP
+    def associated_attr_prediction(self, seq_output, attr_embedding) -> torch.Tensor:
+        """
+        :param seq_output: [B L H]
+        :param attr_embedding: [arribute_num H]
+        :return: scores [B*L tag_num]
+        """
+        seq_output = self.aap_norm(seq_output)  # [B L H]
+        seq_output = seq_output.view([-1, self.base_module.hidden_size, 1])  # [B*L H 1]
+        # [tag_num H] [B*L H 1] -> [B*L tag_num 1]
+        score = torch.matmul(attr_embedding, seq_output)
+        return torch.sigmoid(score.squeeze(-1))  # [B*L tag_num]
+
+    # MIP sample neg items
+    def masked_item_prediction(self, seq_output, target_item) -> torch.Tensor:
+        """
+        :param seq_output: [B L H]
+        :param target_item: [B L H]
+        :return: scores [B*L]
+        """
+        seq_output = self.mip_norm(seq_output.view([-1, self.base_module.hidden_size]))  # [B*L H]
+        target_item = target_item.view([-1, self.base_module.hidden_size])  # [B*L H]
+        score = torch.mul(seq_output, target_item)  # [B*L H]
+        return torch.sigmoid(torch.sum(score, -1))  # [B*L]
+
+    # MAP
+    def masked_attr_prediction(self, seq_output, attr_embedding) -> torch.Tensor:
+        seq_output = self.map_norm(seq_output)  # [B L H]
+        seq_output = seq_output.view([-1, self.base_module.hidden_size, 1])  # [B*L H 1]
+        # [tag_num H] [B*L H 1] -> [B*L tag_num 1]
+        score = torch.matmul(attr_embedding, seq_output)
+        return torch.sigmoid(score.squeeze(-1))  # [B*L tag_num]
+
+    # SP sample neg segment
+    def segment_prediction(self, context, segment) -> torch.Tensor:
+        """
+        :param context: [B H]
+        :param segment: [B H]
+        :return:
+        """
+        context = self.sp_norm(context)
+        score = torch.mul(context, segment)  # [B H]
+        return torch.sigmoid(torch.sum(score, dim=-1))  # [B]
+
+    def forward(
+        self,
+        masked_item_seq,
+        pos_items,
+        neg_items,
+        masked_segment_seq,
+        pos_segment,
+        neg_segment,
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        # Encode masked seq
+        seq_emb = self.base_module.make_seq_embedding(masked_item_seq)
+        seq_mask = (masked_item_seq == 0).float() * -1e8
+        seq_mask = torch.unsqueeze(torch.unsqueeze(seq_mask, 1), 1)
+
+        encoded_layers = self.base_module.item_encoder(seq_emb, seq_mask, output_all_encoded_layers=True)
+        # [B L H]
+        seq_output = encoded_layers[-1]
+
+        attr_embeddings = self.attr_embeddings.weight
+        # AAP
+        aap_score = self.associated_attr_prediction(seq_output, attr_embeddings)
+
+        # MIP
+        pos_item_embs = self.base_module.item_embeddings(pos_items)
+        neg_item_embs = self.base_module.item_embeddings(neg_items)
+        pos_score = self.masked_item_prediction(seq_output, pos_item_embs)
+        neg_score = self.masked_item_prediction(seq_output, neg_item_embs)
+        mip_distance = torch.sigmoid(pos_score - neg_score)
+
+        # MAP
+        map_score = self.masked_attr_prediction(seq_output, attr_embeddings)
+
+        # SP
+        # segment context
+        segment_context = self.base_module.make_seq_embedding(masked_segment_seq)
+        segment_mask = (masked_segment_seq == 0).float() * -1e8
+        segment_mask = torch.unsqueeze(torch.unsqueeze(segment_mask, 1), 1)
+        segment_encoded_layers = self.base_module.item_encoder(segment_context, segment_mask, output_all_encoded_layers=True)
+
+        # take the last position hidden as the context
+        segment_context = segment_encoded_layers[-1][:, -1, :]  # [B H]
+        # pos_segment
+        pos_segment_emb = self.base_module.make_seq_embedding(pos_segment)
+        pos_segment_mask = (pos_segment == 0).float() * -1e8
+        pos_segment_mask = torch.unsqueeze(torch.unsqueeze(pos_segment_mask, 1), 1)
+        pos_segment_encoded_layers = self.base_module.item_encoder(pos_segment_emb, pos_segment_mask, output_all_encoded_layers=True)
+        pos_segment_emb = pos_segment_encoded_layers[-1][:, -1, :]
+
+        # neg_segment
+        neg_segment_emb = self.base_module.make_seq_embedding(neg_segment)
+        neg_segment_mask = (neg_segment == 0).float() * -1e8
+        neg_segment_mask = torch.unsqueeze(torch.unsqueeze(neg_segment_mask, 1), 1)
+        neg_segment_encoded_layers = self.base_module.item_encoder(neg_segment_emb, neg_segment_mask, output_all_encoded_layers=True)
+        neg_segment_emb = neg_segment_encoded_layers[-1][:, -1, :]  # [B H]
+
+        pos_segment_score = self.segment_prediction(segment_context, pos_segment_emb)
+        neg_segment_score = self.segment_prediction(segment_context, neg_segment_emb)
+
+        sp_distance = torch.sigmoid(pos_segment_score - neg_segment_score)
+
+        return aap_score, mip_distance, map_score, sp_distance
