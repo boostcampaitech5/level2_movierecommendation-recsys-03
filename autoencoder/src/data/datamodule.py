79297bcfde763946a76bcd6dc694740afff36cca
alstjrdlzz <tjalstjr95@gmail.com>
2023-06-17 09:17:02 +0000

    fix: change sparse matrix generation logic

--- autoencoder/src/data/datamodule.py
+++ autoencoder/src/data/datamodule.py
@@ -2,12 +2,12 @@ from typing import Optional
 import os
 import pandas as pd
 import lightning as L
+from scipy.sparse import csr_matrix
 from torch.utils.data import DataLoader
 from src.data.dataset import TrainDataset, EvalDataset
 from src.data.utils import (
-    generate_user_item_indexer,
-    split_user_ids,
-    numerize,
+    get_user_seqs,
+    train_val_test_split,
 )
 
 
@@ -21,41 +21,34 @@ class DataModule(L.LightningDataModule):
         self.num_workers = config.data.num_workers
 
         self.raw_data: Optional[pd.DataFrame] = None
-
-        self.indexer = None
+        self.user_seq: Optional[list] = None
+        self.num_items: Optional[int] = None
+        self.raw_matrix: Optional[csr_matrix] = None
+        self.idx2item: Optional[dict] = None
 
         self.train_dataset: Optional[TrainDataset] = None
-        self.valid_dataset: Optional[EvalDataset] = None
+        self.val_dataset: Optional[EvalDataset] = None
         self.test_dataset: Optional[EvalDataset] = None
         self.predict_dataset: Optional[TrainDataset] = None
 
     def prepare_data(self):
         self.raw_data = pd.read_csv(os.path.join(self.train_dir, self.train_file))
-        self.indexer: dict = generate_user_item_indexer(self.raw_data)
+        self.user_seq, self.num_items, self.raw_matrix, self.idx2item = get_user_seqs(self.raw_data)
 
     def setup(self, stage: Optional[str] = None):
-        train_user_ids, valid_user_ids, test_user_ids = split_user_ids(self.raw_data)
+        train_matrix, val_matrix, test_matrix = train_val_test_split(self.raw_matrix)
 
-        train_data = self.raw_data.loc[self.raw_data["user"].isin(train_user_ids)]
-        valid_data = self.raw_data.loc[self.raw_data["user"].isin(valid_user_ids)]
-        test_data = self.raw_data.loc[self.raw_data["user"].isin(test_user_ids)]
-
-        train_data_numerized = numerize(train_data, self.indexer["user2idx"], self.indexer["item2idx"])
-        valid_data_numerized = numerize(valid_data, self.indexer["user2idx"], self.indexer["item2idx"])
-        test_data_numerized = numerize(test_data, self.indexer["user2idx"], self.indexer["item2idx"])
-        raw_data_numerized = numerize(self.raw_data, self.indexer["user2idx"], self.indexer["item2idx"])
-
-        self.train_dataset = TrainDataset(train_data_numerized)
-        self.valid_dataset = EvalDataset(valid_data_numerized)
-        self.test_dataset = EvalDataset(test_data_numerized)
-        self.predict_dataset = TrainDataset(raw_data_numerized)
+        self.train_dataset = TrainDataset(train_matrix)
+        self.val_dataset = EvalDataset(val_matrix)
+        self.test_dataset = EvalDataset(test_matrix)
+        self.predict_dataset = TrainDataset(self.raw_matrix)
 
     def train_dataloader(self) -> DataLoader:
         loader = DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
         return loader
 
     def val_dataloader(self) -> DataLoader:
-        loader = DataLoader(self.valid_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
+        loader = DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers)
         return loader
 
     def test_dataloader(self) -> DataLoader:
--- autoencoder/src/data/utils.py
+++ autoencoder/src/data/utils.py
@@ -4,76 +4,80 @@ import pandas as pd
 from scipy.sparse import csr_matrix
 
 
-def generate_user_item_indexer(data: pd.DataFrame) -> dict:
-    user2idx = {v: k for k, v in enumerate(data["user"].unique())}
-    item2idx = {v: k for k, v in enumerate(data["item"].unique())}
+def generate_rating_matrix(user_seq, num_users, num_items):
+    row = []
+    col = []
+    data = []
+    for user_id, item_list in enumerate(user_seq):
+        for item in item_list:
+            row.append(user_id)
+            col.append(item)
+            data.append(1)
 
-    idx2user = {v: k for k, v in user2idx.items()}
+    row = np.array(row)
+    col = np.array(col)
+    data = np.array(data)
+    rating_matrix = csr_matrix((data, (row, col)), shape=(num_users, num_items))
+
+    return rating_matrix
+
+
+def get_user_seqs(rating_df: pd.DataFrame):
+    item2idx = {v: k for k, v in enumerate(rating_df["item"].unique())}
     idx2item = {v: k for k, v in item2idx.items()}
 
-    indexer = {
-        "user2idx": user2idx,
-        "item2idx": item2idx,
-        "idx2user": idx2user,
-        "idx2item": idx2item,
-    }
+    rating_df["item"] = rating_df["item"].apply(lambda x: item2idx[x])
 
-    return indexer
+    lines = rating_df.groupby("user")["item"].apply(list)
+    user_seq = []
+    item_set = set()
+    for line in lines:
+        items = line
+        user_seq.append(items)
+        item_set = item_set | set(items)
 
+    num_users = len(lines)
+    num_items = len(item_set)
 
-def split_user_ids(data: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    user_ids = data["user"].unique()
-    num_users = data["user"].nunique()
+    rating_matrix = generate_rating_matrix(user_seq, num_users, num_items)
 
-    train_cut = int(num_users * 0.8)
-    valid_cut = train_cut + int(num_users * 0.1)
-
-    shuffled_user_ids = user_ids[np.random.permutation(num_users)]
-
-    train_user_ids = shuffled_user_ids[:train_cut]
-    valid_user_ids = shuffled_user_ids[train_cut:valid_cut]
-    test_user_ids = shuffled_user_ids[valid_cut:]
-
-    assert set(train_user_ids) & set(valid_user_ids) & set(test_user_ids) == set()
-
-    return train_user_ids, valid_user_ids, test_user_ids
+    return (
+        user_seq,
+        num_items,
+        rating_matrix,
+        idx2item,
+    )
 
 
-def numerize(data: pd.DataFrame, user2idx: dict, item2idx: dict) -> pd.DataFrame:
-    user_id = data["user"].apply(lambda x: user2idx[x])
-    item_id = data["item"].apply(lambda x: item2idx[x])
-    return pd.DataFrame(data={"user_id": user_id, "item_id": item_id}, columns=["user_id", "item_id"])
+def train_val_test_split(sparse_matrix, ratio_list=[0.8, 0.1, 0.1]):
+    num_users = sparse_matrix.shape[0]
+
+    train_cut = int(num_users * ratio_list[0])
+    val_cut = train_cut + int(num_users * ratio_list[1])
+
+    train = sparse_matrix[:train_cut]
+    val = sparse_matrix[train_cut:val_cut]
+    test = sparse_matrix[val_cut:]
+
+    return (train, val, test)
 
 
-def generate_sparse_matrix(df: pd.DataFrame) -> csr_matrix:
-    num_users = df["user_id"].max() + 1
-    num_items = 6807
-
-    row = df["user_id"].values
-    col = df["item_id"].values
-    data = np.ones_like(row)
-
-    sparse_matrix = csr_matrix((data, (row, col)), dtype="float64", shape=(num_users, num_items))
-
-    return sparse_matrix
-
-
-def split_sparse_matrix_stratified(sparse_matrix: csr_matrix, ratio: float = 0.2) -> Tuple[csr_matrix, csr_matrix]:
+def input_target_split(sparse_matrix: csr_matrix, ratio: float = 0.2) -> Tuple[csr_matrix, csr_matrix]:
     numpy_matrix: np.ndarray = sparse_matrix.toarray()
 
     input_matrix = numpy_matrix.copy()
     target_matrix = numpy_matrix.copy()
 
-    num_rated: np.ndarray = np.sum(input_matrix != 0, axis=1)  # 유저 별 평가 횟수 계산
-    test_size: np.ndarray = (num_rated * ratio).astype(int)  # 유저 별 test_size 계산
+    num_rated: np.ndarray = np.sum(input_matrix != 0, axis=1)
+    target_size: np.ndarray = (num_rated * ratio).astype(int)
 
     for user_id in range(numpy_matrix.shape[0]):
         idx: list = np.asarray(np.where(input_matrix[user_id] != 0))[0].tolist()
-        idx_test: list = np.random.choice(idx, test_size[user_id], replace=False).tolist()  # test_size만큼 비복원추출된 아이템 리스트
-        idx_train: list = list(set(idx) - (set(idx_test)))  # 추출되지 않은 나머지 아이템 리스트
+        idx_target: list = np.random.choice(idx, target_size[user_id], replace=False).tolist()
+        idx_input: list = list(set(idx) - (set(idx_target)))
 
-        input_matrix[user_id, idx_test] = 0  # test로 추출된 아이템 0으로 변경
-        target_matrix[user_id, idx_train] = 0  # train에서 이미 평가된 아이템 0으로 변경
+        input_matrix[user_id, idx_target] = 0
+        target_matrix[user_id, idx_input] = 0
 
     assert input_matrix.shape == target_matrix.shape
 
