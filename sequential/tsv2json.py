20420fdc02a53e6b5b2408d372fa90a669ef3f91
2jun0 <soo28819@gmail.com>
2023-06-18 13:29:43 +0000

    fix: load tsv from in-project folder

--- sequential/tsv2json.py
+++ sequential/tsv2json.py
@@ -4,14 +4,15 @@ import pandas as pd
 
 
 DATA_VERSION = "V1"
-BASE_PATH = "data/train"
+GLOBAL_BASE_PATH = "../data/train"
+LOCAL_BASE_PATH = "./data/train"
 
 
 def tsv2json(file_name):
-    file_path = os.path.join(BASE_PATH, file_name)
+    file_path = os.path.join(GLOBAL_BASE_PATH, file_name)
     df = pd.read_csv(file_path, delimiter="\t")
 
-    item2idx_path = os.path.join(BASE_PATH, "item2idx.json")
+    item2idx_path = os.path.join(LOCAL_BASE_PATH, "item2idx.json")
 
     with open(item2idx_path) as file:
         item2idx = json.load(file)
@@ -27,13 +28,13 @@ def tsv2json(file_name):
         attrs = group[attr_col].cat.codes.to_list()
         json_dict[item] = attrs
 
-    json_path = os.path.join(BASE_PATH, f"{DATA_VERSION}_item2{file_name[:-4]}.json")
+    json_path = os.path.join(LOCAL_BASE_PATH, f"{DATA_VERSION}_item2{file_name[:-4]}.json")
     with open(json_path, "w") as f:
         json.dump(json_dict, f)
 
 
 if __name__ == "__main__":
-    for file_name in os.listdir(BASE_PATH):
+    for file_name in os.listdir(GLOBAL_BASE_PATH):
         if not file_name.endswith(".tsv"):
             continue
 
