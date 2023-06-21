620b264bfff358c9dda69ab327942a5869404ea8
juhyein <hyeinju304@gmail.com>
2023-06-21 08:59:13 +0000

    fix: change config that converted to absolute path

--- recbole/src/trainer.py
+++ recbole/src/trainer.py
@@ -1,7 +1,6 @@
-import numpy as np
 from ray import tune
-from typing import Dict, Any, Optional, Tuple
 from recbole.config import Config
+from typing import Dict, Any, Optional, Tuple
 from recbole.utils import init_logger, init_seed
 from recbole.utils.utils import get_model, get_trainer
 from recbole.data import create_dataset, data_preparation
@@ -13,8 +12,8 @@ def get_scheduler() -> ASHAScheduler:
     return scheduler
 
 
-def train_recbole(config_file_list: list, config: Optional[dict] = None) -> Tuple[str, float, dict[str, float], float, str]:
-    config = Config(config_file_list=config_file_list, config_dict=config)
+def train_recbole(config: dict = None) -> Tuple[str, float, dict[str, float], float, str]:
+    config = Config(config_dict=config)
     init_seed(config["seed"], config["reproducibility"])
     init_logger(config)
 
@@ -26,15 +25,15 @@ def train_recbole(config_file_list: list, config: Optional[dict] = None) -> Tupl
     model = get_model(model_name)(config, train_data.dataset).to(config["device"])
 
     trainer = get_trainer(config["MODEL_TYPE"], config["model"])(config, model)
-    best_valid_score, best_valid_result = trainer.fit(train_data, valid_data, verbose=False)
+    best_valid_score, best_valid_result = trainer.fit(train_data, valid_data, show_progress=config["show_progress"], verbose=False)
     test_result = trainer.evaluate(test_data)
     model_file = trainer.saved_model_file
 
     return model_name, best_valid_score, best_valid_result, test_result, model_file
 
 
-def objective_function(config: None, config_file_list: list) -> Dict[str, Any]:
-    model_name, best_valid_score, best_valid_result, test_result, _ = train_recbole(config_file_list=config_file_list)
+def objective_function(config: None, config_dict: dict) -> Dict[str, Any]:
+    model_name, best_valid_score, best_valid_result, test_result, _ = train_recbole(config=config_dict)
     tune.report(recall=best_valid_score)
 
     return {"model": model_name, "recall": best_valid_score, "best_valid_result/recall@10": best_valid_result, "test_result": test_result}
